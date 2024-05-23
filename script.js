fcca9f2029204e7b4031d061c5f0d93127020699
m-s1621 <mebashimelis@gmail.com>
2024-05-23 15:08:34 -0400

    update p1 and p2

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ let cell1 = "";
 let cell2 = "";
 let cell3 = "";
 let cell4 = "";
-let str = "ID,Name,Occupation,Age\\n42,Bruce,Knight,41\\n57,Bob,Fry Cook,19\\n63,Blaine,Quiz Master,58\\n98,Bill,Doctor's Assistant,26";
+let str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
+let charLength = str.length
 let commaCount = 1;
 let arr = [];
-let row = [];
+
+
 for (char of str) {
     if (commaCount == 4) {
-        if (char == "\\") {
+        if (char == "\n") {
             const row = {
                 cell1: cell1,
                 cell2: cell2,
@@ -17,16 +19,14 @@ for (char of str) {
                 cell4: cell4
             }
             arr.push({ ...row });
-        }
-        cell4 += char
-        if (char == 'n') {
             commaCount = 1;
             cell1 = "";
             cell2 = "";
             cell3 = "";
             cell4 = "";
         }
-       
+        cell4 += char
+
     } else {
         if (char == ',')
             commaCount++
@@ -41,39 +41,36 @@ for (char of str) {
     }
     charLength--
 };
-for (let i = 0; i < arr.length; i++) {
-    console.log(`
-        ${arr[i].cell1},  ${arr[i].cell2},  ${arr[i].cell3},  ${arr[i].cell4},
-`)
-}
+// console.log(arr)
+
 
 // Part 2: Expanding Functionality
+let row3 = [];
+let arr2 = []
 for (let char of str) {
-    if (char === '\\') {
-        
-        if (str[str.indexOf(char) + 1] === 'n') {
-            row.push(cell4);
-            arr.push([...row]);
-            row = [];
+    if (char == '\n') {
+        row3.push(cell4);
+        arr2.push([...row3]);
+        row3 = [];
+        cell1 = "";
+        cell2 = "";
+        cell3 = "";
+        cell4 = "";
+        commaCount = 1;
+
+
+    } else if (char == ',') {
+        if (commaCount == 1) {
+            row3.push(cell1);
             cell1 = "";
+        } else if (commaCount == 2) {
+            row3.push(cell2);
             cell2 = "";
-            cell3 = "";
-            cell4 = "";
-            commaCount = 1;
-            continue;
-        }
-    } else if (char === ',') {
-        commaCount++;
-        if (commaCount == 2) {
-            row.push(cell1);
-            cell1 = "";
         } else if (commaCount == 3) {
-            row.push(cell2);
-            cell2 = "";
-        } else if (commaCount == 4) {
-            row.push(cell3);
+            row3.push(cell3);
             cell3 = "";
         }
+        commaCount++;
     } else {
         if (commaCount == 1) {
             cell1 += char;
@@ -88,10 +85,9 @@ for (let char of str) {
 }
 
 if (cell1 || cell2 || cell3 || cell4) {
-    row.push(cell4);
-    arr.push([...row]);
+    row3.push(cell4);
+    arr2.push([...row3]);
 }
 
-console.log(JSON.stringify(arr, null, 2));
+// console.log(arr2)
 
-// Part 3: Transforming Data
